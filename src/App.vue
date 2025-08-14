001afcbf3bb505c82456a2d350780eaf6adb63ce
xzha0459 <xzha0459@student.monash.edu>
2025-08-14 16:16:58 +0800

    add Form.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-import JSONLab from './components/JSONLab.vue'
+
+import JSONLab from '@/components/JSONLab.vue'
 </script>
 
 <template>
@@ -9,30 +10,9 @@ import JSONLab from './components/JSONLab.vue'
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+#app {
+  margin: 0;
+  padding: 0;
+  min-height: 100vh;
 }
 </style>
